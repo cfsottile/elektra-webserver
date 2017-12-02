1e64f8bc1ebda6d34559b30073b7a43f27e44373
Cristian Sottile <cfsottile@gmail.com>
2017-12-02 14:25:37 -0300

    Updated old models

--- models/measure.rb
+++ models/measure.rb
@@ -12,15 +12,78 @@ class Measure
     measures.each { |m| self.store(m) }
   end
 
+  def self.last_for(sensor_name)
+    $db["last_measures"].find({sensor_name: sensor_name}).projection({_id: 0}).to_a.first
+  end
+
+  def self.consumption_from_to(sensor_name, raw_from, raw_to, precision)
+    averaged_measures(sensor_name, DateTime.rfc3339(raw_from), DateTime.rfc3339(raw_to), precision)
+      .to_a
+      .map {|m| format_averaged_measure(m)}
+  end
+
   private
   def self.validate(measure)
-    Sensor.exists(measure["sensor_id"])
+    Sensor.exists(name: measure["sensor_name"])
   end
 
   def self.update_last_measure(measure)
     $db[:last_measures].update_one(
-      {"sensor_id" => measure["sensor_id"]},
+      {"sensor_name" => measure["sensor_name"]},
       {"$set" => measure},
       upsert: true)
+  end
+
+  PRECISION_GROUP = {
+      "hour" => { "$hour" => "$time" },
+      "day" => { "$dayOfMonth" => "$time" },
+      "month" => { "$month" => "$time" },
+      "year" => { "$year" => "$time" }
+  }
+
+  def self.precision_query(precision)
+    PRECISION_GROUP.drop_while {|k,v| k != precision}.to_h
+  end
+
+  PRECISION_FORMAT = {
+    "hour" => "%H",
+    "day" => "%d",
+    "month" => "%m",
+    "year" => "%Y"
+  }
+
+  def self.format_date(precision)
+    PRECISION_FORMAT
+      .drop_while {|k,v| k != precision}
+      .to_h
+      .values
+      .reverse
+      .join("-")
+  end
+
+  OFFSET = 3 * 60 * 60 * 1000
+
+  def averaged_measures(id, from, to, precision)
+    $db["measures"]
+      .find({sensor_name: id})
+      .aggregate([
+        { "$match" => { "time" => { "$gt" => from, "$lt" => to}}},
+        { "$group" => {
+          "_id" => precision_query(precision),
+          "avg" => { "$avg" => "$value" },
+          "time" => { "$first" => "$time" } }},
+        { "$sort" => { "time" => 1 }},
+        { "$project" => {
+          "time" => { "$subtract" => ["$time", OFFSET]},
+          "avg" => 1}},
+        { "$project" => {
+          "date" => {
+            "$dateToString" => { "format" => format_date(precision), "date" => "$time" }},
+            "avg" => 1}}
+        ])
+  end
+
+  def self.format_averaged_measure(measure)
+    { value: measure["avg"], date: measure["date"] }
   end
 end
--- models/sensor.rb
+++ models/sensor.rb
@@ -1,128 +1,46 @@
-class Sensor
-  def self.json_show(id)
-    gen_json_sensor(find_with_data(id))
+require_relative "base_model"
+require_relative "device"
+require_relative "measure"
+
+class Sensor < BaseModel
+  belongs_to :device
+
+  validates :name, presence: true, uniqueness: true, length: { in: 1..30 }
+  validates :description, presence: true, length: { in: 1..50 }
+  validates :status, inclusion: { within: [0,1] }
+  validates :device, presence: true
+
+  def consumption_from_to(raw_from, raw_to, precision)
+    Measure.consumption_from_to(
+      self.name,
+      DateTime.rfc3339(raw_from),
+      DateTime.rfc3339(raw_to),
+      precision)
   end
 
-  def self.json_last_measure(id)
-    last_measure_for(id)
-      .to_a
-      .first
-      .to_json
-  end
-
-  def self.json_index
-    all_with_data
-      .to_a
-      .map { |s| gen_json_sensor(s) }
-      .to_json
-  end
-
-  def self.exists(id)
-    $db["sensors"].find({id: id}).to_a.first != nil
-  end
-
-  def self.consumption_from_to(id, raw_from, raw_to, precision)
-    averaged_measures(id, DateTime.rfc3339(raw_from), DateTime.rfc3339(raw_to), precision)
-      .to_a
-      .map {|m| format_averaged_measure(m)}
-      .to_json
+  def last_measure
+    Measure.last_for(self.name)
   end
 
   private
-  def self.gen_json_sensor(sensor_data)
-    result = {
-      id: sensor_data["id"],
-      name: sensor_data["name"],
-      status: sensor_data["status"] == 1 ? "on" : "off",
-      # location: $db["places"].find({id: sensor_data["mote"].first["location"]}).to_a.first["name"],
-      # mote_id: sensor_data["mote"].first["id"],
-      mote_name: sensor_data["mote"].first["name"]
+  def hash_data
+    {
+      name: name,
+      description: description,
+      status: status
     }
-    if sensor_data["last_measure"].first
-      result[:last_measure] = {
-        time: sensor_data["last_measure"].first["time"],
-        value: sensor_data["last_measure"].first["value"]
-      }
-    end
-    result.to_json
   end
 
-  LAST_MEASURES_AGGREGATION =
-    {"$lookup" =>
-      {from: "last_measures", localField: "sensor_id", foreignField: "id", as: "last_measure"}
+  def hash_last
+    { last_measure: last_measure }
+  end
+
+  def hash_assoc
+    {
+      device_name: device.name,
+      device_description: device.description,
+      place_name: device.place.name,
+      place_description: device.place.description,
     }
-  MOTES_AGGREGATION =
-    {"$lookup" =>
-      {from: "motes", localField: "mote_id", foreignField: "id", as: "mote"}
-    }
-
-  def self.find_with_data(id)
-    $db["sensors"]
-      .aggregate([
-        MOTES_AGGREGATION,
-        LAST_MEASURES_AGGREGATION,
-        { "$match" => { "id" => id }}])
-      .to_a.first
-  end
-
-  def self.all_with_data
-    $db["sensors"].aggregate([MOTES_AGGREGATION, LAST_MEASURES_AGGREGATION])
-  end
-
-  def self.last_measure_for(id)
-    $db["last_measures"].find({sensor_id: id})
-  end
-
-  PRECISION_GROUP = { 
-    "hour" => { "$hour" => "$time" },
-    "day" => { "$dayOfMonth" => "$time" },
-    "month" => { "$month" => "$time" },
-    "year" => { "$year" => "$time" }
-  }
-
-  def self.precision_query(precision)
-    PRECISION_GROUP.drop_while {|k,v| k != precision}.to_h
-  end
-
-  PRECISION_FORMAT = { 
-    "hour" => "%H",
-    "day" => "%d",
-    "month" => "%m",
-    "year" => "%Y"
-  }
-
-  def self.format_date(precision)
-    PRECISION_FORMAT
-      .drop_while {|k,v| k != precision}
-      .to_h
-      .values
-      .reverse
-      .join("-")
-  end
-
-  OFFSET = 3 * 60 * 60 * 1000
-
-  def self.averaged_measures(id, from, to, precision)
-    $db["measures"]
-      .find({sensor_id: id})
-      .aggregate([
-        { "$match" => { "time" => { "$gt" => from, "$lt" => to}}},
-        { "$group" => {
-          "_id" => precision_query(precision),
-          "avg" => { "$avg" => "$value" },
-          "time" => { "$first" => "$time" } }},
-        { "$sort" => { "time" => 1 }},
-        { "$project" => { 
-          "time" => { "$subtract" => ["$time", OFFSET]},
-          "avg" => 1}},
-        { "$project" => { 
-          "date" => { 
-            "$dateToString" => { "format" => format_date(precision), "date" => "$time" }},
-          "avg" => 1}}
-      ])
-  end
-
-  def self.format_averaged_measure(measure)
-    { value: measure["avg"], date: measure["date"] }
   end
 end

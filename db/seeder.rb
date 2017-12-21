1ac34c56bbd989c4e9e34d322815c594b16d3219
Cristian Sottile <cfsottile@gmail.com>
2017-12-20 21:53:36 -0300

    Added last_measures seeds

--- db/seeder.rb
+++ db/seeder.rb
@@ -4,9 +4,10 @@ Mongo::Logger.logger.level = Logger::FATAL
 db = Mongo::Client.new(["localhost:27017"], database: "iot_db")
 
 values = [3.5,3.6,3.7,3.8,3.9,4.0,4.1]
-sensor_codes = ["a9d1s1"]
+sensor_codes = ["a5d1s1"]
 dates = (DateTime.now << 1).step(DateTime.now, Rational(1,24*60*30))
 
+db["measures"].drop
 db["measures"].insert_many(
   (sensor_codes.map do |sensor|
     dates.map do |date|
@@ -15,26 +16,9 @@ db["measures"].insert_many(
   end).flatten
 )
 
-#
-# db["last_measures"].insert_many([
-#   {
-#     "sensor_code" => "a9d1s1",
-#     "time" => Time.at(1470019800).utc.to_datetime,
-#     "value" => 7.9
-#   },
-#   {
-#     "sensor_code" => "a9d1s2",
-#     "time" => Time.at(1470019800).utc.to_datetime,
-#     "value" => 2.7
-#   },
-#   {
-#     "sensor_code" => "a5d1s1",
-#     "time" => Time.at(1470019800).utc.to_datetime,
-#     "value" => 4.1
-#   },
-#   {
-#     "sensor_code" => "a5d1s2",
-#     "time" => Time.at(1470019800).utc.to_datetime,
-#     "value" => 6.4
-#   }
-# ])
+db["last_measures"].drop
+db["last_measures"].insert_many(
+  sensor_codes.map do |s|
+    db["measures"].find({sensor_code: s}).sort({time: -1}).limit(1).to_a.first
+  end
+)
